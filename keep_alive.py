c6ac23e7a77ffb169ef6930d36dad48d530f453a
etrasr <emrumer@gmail.com>
2025-11-17 02:59:10 +0300

    Update response message and set thread as daemon

--- keep_alive.py
+++ keep_alive.py
@@ -1,3 +1,4 @@
+# keep_alive.py
 from flask import Flask
 from threading import Thread
 
@@ -5,11 +6,12 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "I'm alive!"
+    return "Bot is running!"
 
 def run():
     app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
     t = Thread(target=run)
+    t.daemon = True
     t.start()
